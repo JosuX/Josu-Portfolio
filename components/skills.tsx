e83c08c3b5c4e0139a2d3231b32f81be2f6ef64b
JosuX <joferusa43@gmail.com>
2023-07-13 18:21:39 +0800

    Edit description and minor changes

--- components/skills.tsx
+++ components/skills.tsx
@@ -12,7 +12,7 @@ const Skills = () => {
     <div id='skills' className='pt-20 bg-[#181818] w-screen text-[#F5F5F5] flex flex-col items-center pb-6'>
         <div className={'mb-16 text-6xl font-semibold ' + crimson.className}>Skills</div>
         <div className='flex flex-row align-top items-start gap-20'>
-        <div className=''>
+        <div className='align-center'>
         <Image src={SkillsGIF} width={400} height={400} alt='skills'/>
         </div>
         <div className='flex flex-col w-[584px] h-56 justify-between pt-10'>
