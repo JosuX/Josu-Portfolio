22a8739bf0a8fd722258aa72f1467d2c34d99410
JosuX <joferusa43@gmail.com>
2023-07-13 19:11:25 +0800

    Add Contact Photo

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import ContactImage from '../assets/contact.jpg'
 import { Crimson_Pro, Inconsolata, Inter } from 'next/font/google'
 
 const crimson = Crimson_Pro({ subsets: ['latin']})
@@ -8,19 +10,24 @@ const inter = Inter({ subsets: ['latin']})
 const Contact = () => {
   return (
     <div id='contact' className='flex items-center content-center justify-center h-[46vw] w-screen bg-[#4C346B] text-[#F5F5F5]'>
-        <div className='flex flex-row justify-between h-[33vw] w-[79vw]'>
-        <div className='flex flex-col justify-between'>
-        <h2 className={'text-6xl font-semibold ' + crimson.className}>Who's Jofer?</h2>
-        <h3 className={'text-2xl w-[619px] ' + inconsolata.className}>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ma</h3>
+        <div className='flex flex-row justify-between h-[33vw] w-[79vw] gap-6'>
+        <div className='flex flex-col justify-between gap-6'>
+        <h2 className={'text-5xl font-semibold ' + crimson.className}>Who's Jofer?</h2>
+        <h3 className={'text-xl w-[500px] text-justify ' + inconsolata.className}>A developer committed to utilizing technology to make a positive impact on society, deeply passionate about the transformative power of technology to enhance people's lives and having the ultimate goal to become an accomplished developer capable of creating innovative solutions that solve complex problems.</h3>
         <div className='flex flex-col '>
-        <h4 className={'text-xl font-semibold mb-[20px] ' + crimson.className}>Who's Jofer?</h4>
-        <h5 className={'text-lg font-medium w-[519px] ' + inconsolata.className}>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ma</h5>
+        <h4 className={'text-lg font-semibold mb-[20px] ' + crimson.className}>Let's Connect!</h4>
+        <h5 className={'text-md font-medium w-[519px] ' + inconsolata.className}>
+            <a href = "mailto: abc@example.com">joferusa43@gmail.com</a>
+            <span>&nbsp;|&nbsp;</span>
+            <a href="tel:+639274659570">+639274659570</a>
+        </h5>
         </div>
         <a href='/resume.pdf' className={'text-lg font-medium underline ' + inter.className}>
             View my resume
         </a>
         </div>
-        <div className='shrink-0 bg-white w-[415px] h-[33vw]'>
+        <div className='relative shrink-0 w-[415px] h-[33vw]'>
+            <Image src={ContactImage} layout='fill' objectFit='cover' alt='image'/>
 
         </div>
         
