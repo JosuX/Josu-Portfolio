6b91158fb52da406b8fdeb7e1320cabead694784
JosuX <joferusa43@gmail.com>
2023-07-16 00:46:48 +0800

    API Photo Count fix

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,11 +1,12 @@
 import fs from 'fs';
+import path from 'path'
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const query = new URL(request.url).searchParams;
-  const queryParam = query.get('id');
+  const id = query.get('id');
   try {
-    const files = fs.readdirSync('./public/projects/' + queryParam);
+    const files = fs.readdirSync(path.join(process.cwd(), 'projects', id!));
     const fileCount = files.length;
     return new NextResponse(JSON.stringify({ count: fileCount }));
   } catch (error) {
